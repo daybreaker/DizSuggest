c4e19b29786d9bf524623f8e2d679226658fbc15
daybreaker <daybreaker@gmail.com>
2011-04-09 19:05:00 -0500

    Init with jquery, make voteable gem'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dizsuggest::Application.config.secret_token = '1d11e9e472aaccb142a50b8a1e394745116faab923685dbfbcdcdd612fb12def586e6860a75f8315467b4aba588445efa3017f51a22b9137733c13b27957dde5'
