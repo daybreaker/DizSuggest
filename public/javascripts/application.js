c389a7312dac35e1cda03596c363c7bcae5ed627
Brad Huber <bradhuber@gmail.com>
2011-05-29 22:21:02 -0500

    Width changed to 100%, not 1024px, plus many other layout changes related to that.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,23 +1,23 @@
 $(function(){
   $('a.upvote').live('click', function(){
-    parent = $(this).parents('p');
+    parent = $(this).parents('div.votes');
     id = $('input.idea', parent).val();
     url = (parent.hasClass('index')) ? '/' : '/ideas/'+id;
 
     $.post('/ideas/' + id + '/upvote', {id:id}, function(){
-      parent.load(url + ' p#votes_'+ id, function(){
+      parent.load(url + ' div#votes_'+ id, function(){
 				$(".total_votes").textfill({maxFontSize: 60});
 			});
     });
   });
   
   $('a.downvote').live('click', function(){
-    parent = $(this).parents('p');
+    parent = $(this).parents('div.votes');
     id = $('input.idea', parent).val();
     url = (parent.hasClass('index')) ? '/' : '/ideas/'+id;
 
     $.post('/ideas/' + id + '/downvote', {id:id}, function(){
-			parent.load(url + ' p#votes_'+ id, function(){
+			parent.load(url + ' div#votes_'+ id, function(){
 				$(".total_votes").textfill({maxFontSize: 60});
 			});
     });
