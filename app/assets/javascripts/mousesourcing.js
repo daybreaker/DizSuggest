8cdfbe5972aca4a57974325dafbb056079096b7b
Brad Huber <bhuber@iseatz.com>
2011-10-10 09:07:48 -0500

    New design coming along pretty good I think. Still no substantial code changes.

--- app/assets/javascripts/mousesourcing.js
+++ app/assets/javascripts/mousesourcing.js
@@ -25,7 +25,7 @@ $(function(){
   
   $('#flash_notice').fadeIn().delay(3500).fadeOut();
    
-	$(".total_votes").textfill({maxFontSize: 60});
+//	$(".total_votes").textfill({maxFontSize: 60});
 	
 	$(".reply").click(function(){
 		reply = $(this);
@@ -34,12 +34,30 @@ $(function(){
 		});
 		return false;
 	});
-	
+	/*
 	$( ".tag_cloud" ).find('input.tag').tagedit({
 //			autocompleteURL: 'server/autocomplete.php',
 			allowEdit: false,
 			allowAdd: false
 		});
+		*/
+	$('.filter-trigger').click(function(){
+	  var $fieldsets = $("#filter_list .fieldset h3 span");
+	  var $postFunction = function(){};
+	  
+	  if (!$("#filter").hasClass('active')) {
+	    $('#filter_list').slideToggle(500,function(){
+	      $fieldsets.each(function(){
+	        if(!$(this).hasClass('active')) $(this).addClass('active');
+	      });
+	    });
+	  } else {
+	    $('#filter_list').slideToggle(500);
+	    $fieldsets.removeClass('active');
+	  }
+
+	  $("#filter").toggleClass('active');
+	});
 	
 });
 
